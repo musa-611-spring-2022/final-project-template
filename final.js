0878cd05e92acbde85f83a211282f33769da0c51
sighuh <ssheth112@gmail.com>
2022-05-04 09:49:08 -0400

    added laal icon

--- final.js
+++ final.js
@@ -1,7 +1,13 @@
 let map = L.map('map').setView([40.751,-73.798], 9.95); //zoomed to NYC
 let layerGroup = L.layerGroup().addTo(map);
 
-L.marker([40.87369498325355, -73.880724989077587]).addTo(map);
+var laalIcon = L.icon({
+  iconUrl: 'laal logo JPG.jpg',
+  iconSize:     [38, 95], // size of the icon
+  iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+  popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+L.marker([40.87369498325355, -73.880724989077587], {icon: laalIcon}).addTo(map);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/saisheth/cl1nsj746003g15nz6hd6pqvs/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic2Fpc2hldGgiLCJhIjoiY2wwcjJscXdyMmdsbDNlcWt0eWQ3NHh4bCJ9._YM5R9AGKiS7q_v80O8NVQ', {
 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
