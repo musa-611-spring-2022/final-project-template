a6db0ce5655c39b12081ea9ba1669feaa98a0d54
sighuh <ssheth112@gmail.com>
2022-05-05 11:35:51 -0400

    updating slides and final js

--- final3.js
+++ final3.js
@@ -2,6 +2,12 @@ let map = L.map('map').setView([40.751,-73.798], 9.95); //zoomed to NYC
 L.tileLayer('https://api.mapbox.com/styles/v1/saisheth/cl1nsj746003g15nz6hd6pqvs/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic2Fpc2hldGgiLCJhIjoiY2wwcjJscXdyMmdsbDNlcWt0eWQ3NHh4bCJ9._YM5R9AGKiS7q_v80O8NVQ', {
 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+let layerGroup = L.layerGroup().addTo(map);
+
+var popup = L.popup()
+    .setLatLng([40.87369498325355, -73.880724989077587])
+    .setContent('<center><img src=laalBaari.png style="width:90px;height:100px;"></center>')
+    .openOn(map);
 
 let slideNumber = 0;
 let currentLayer = new L.LayerGroup().addTo(map);
@@ -23,7 +29,7 @@ const fetchMapData = () => {
   .then(data => {
     slides = data;
     showMapData(data);
-    showCurrentSlide();
+    showSlide();
     console.log(data)
   });
 }
@@ -90,3 +96,13 @@ slides.forEach((slide, i) => {
 window.addEventListener('load', () => {
   showSlide(slideNumber);
 });
+
+//added legend to the map, added in css
+let legend = L.control({position: "bottomleft"});
+legend.onAdd = function() {
+    let div = L.DomUtil.create("div", "legend");
+    div.innerHTML = 
+        '<p><b>Click on the map to view the data!</b></p><hr>';
+    return div;
+};
+legend.addTo(map);
--- slides.js
+++ slides.js
@@ -2,7 +2,7 @@ const slides = [
     {
       title: 'NYC',
       content: 'Bangladeshis are one of the fastest growing immigrant communities in NYC and the poorest compared to other Asian ethnic communities. According to a 2022 report by DOHMH on AAPI Health, 58% of Bangladeshi's in NYC earn an income 200% below the federal poverty level and Bangladeshi households in NYC are the most rent burdened of AAPI New Yorkers, 70% pay over 30% of their income on housing.Like most immigrant communities, this population faces a range of economic and social stressors, including financial strains, discrimination, inadequate housing, and social isolation.',
-      bounds: [[10.546875, 48.253941, 36.298828, 57.774518]],
+      bounds: [[40.751,-73.798]],
       features: L.geoJSON(countries, {
         filter: (feature) => feature.properties.title === 'NYC',
         onEachFeature(feature, layer) {
@@ -14,7 +14,7 @@ const slides = [
     {
       title: 'Bronx',
       content: `The Bronx is home to a large Bangladeshi community, mostly located in Parkchester, Grand Concourse and Norwood. Recent ACS data (2016-2020) estimated 14,918 foreign born Bangladeshis and 10,891 US-born Bangladeshis.`,
-      bounds: [[10.546875, 48.253941, 36.298828, 57.774518]],
+      bounds: [[40.751,-73.798]],
       features: L.geoJSON(countries, {
         filter: (feature) => feature.properties.title === 'Bronx',
         onEachFeature(feature, layer) {
@@ -29,6 +29,102 @@ const slides = [
       bounds: [[10.546875, 48.253941, 36.298828, 57.774518]],
       features: L.geoJSON(slides, {
         filter: (feature) => feature.properties.title === 'Community District 7',
+        onEachFeature(feature, layer) {
+          layer.setStyle({  color: '##fb2828', fillOpacity: 0.4 });
+          layer.bindPopup(`<h4><strong>${feature.properties.NAME}</strong></h4>`);
+        },
+      }),
+    },
+    {
+      title: 'Census Tract: 425',
+      content: ``,
+      bounds: [[10.546875, 48.253941, 36.298828, 57.774518]],
+      features: L.geoJSON(slides, {
+        filter: (feature) => feature.properties.title === 'Census Tract:425',
+        onEachFeature(feature, layer) {
+          layer.setStyle({  color: '##fb2828', fillOpacity: 0.4 });
+          layer.bindPopup(`<h4><strong>${feature.properties.NAME}</strong></h4>`);
+        },
+      }),
+    },
+    {
+      title: 'Census Tract: 419',
+      content: ``,
+      bounds: [[10.546875, 48.253941, 36.298828, 57.774518]],
+      features: L.geoJSON(slides, {
+        filter: (feature) => feature.properties.title === 'Census Tract:419',
+        onEachFeature(feature, layer) {
+          layer.setStyle({  color: '##fb2828', fillOpacity: 0.4 });
+          layer.bindPopup(`<h4><strong>${feature.properties.NAME}</strong></h4>`);
+        },
+      }),
+    },
+    {
+      title: 'Census Tract: 421',
+      content: ``,
+      bounds: [[10.546875, 48.253941, 36.298828, 57.774518]],
+      features: L.geoJSON(slides, {
+        filter: (feature) => feature.properties.title === 'Census Tract: 421',
+        onEachFeature(feature, layer) {
+          layer.setStyle({  color: '##fb2828', fillOpacity: 0.4 });
+          layer.bindPopup(`<h4><strong>${feature.properties.NAME}</strong></h4>`);
+        },
+      }),
+    },
+    {
+      title: 'Census Tract: 431.01',
+      content: ``,
+      bounds: [[10.546875, 48.253941, 36.298828, 57.774518]],
+      features: L.geoJSON(slides, {
+        filter: (feature) => feature.properties.title === 'Census Tract:431.01',
+        onEachFeature(feature, layer) {
+          layer.setStyle({  color: '##fb2828', fillOpacity: 0.4 });
+          layer.bindPopup(`<h4><strong>${feature.properties.NAME}</strong></h4>`);
+        },
+      }),
+    },
+    {
+      title: 'Census Tract: 423',
+      content: ``,
+      bounds: [[10.546875, 48.253941, 36.298828, 57.774518]],
+      features: L.geoJSON(slides, {
+        filter: (feature) => feature.properties.title === 'Census Tract:423',
+        onEachFeature(feature, layer) {
+          layer.setStyle({  color: '##fb2828', fillOpacity: 0.4 });
+          layer.bindPopup(`<h4><strong>${feature.properties.NAME}</strong></h4>`);
+        },
+      }),
+    },
+    {
+      title: 'Census Tract: 429.02',
+      content: ``,
+      bounds: [[10.546875, 48.253941, 36.298828, 57.774518]],
+      features: L.geoJSON(slides, {
+        filter: (feature) => feature.properties.title === 'Census Tract: 429.02',
+        onEachFeature(feature, layer) {
+          layer.setStyle({  color: '##fb2828', fillOpacity: 0.4 });
+          layer.bindPopup(`<h4><strong>${feature.properties.NAME}</strong></h4>`);
+        },
+      }),
+    },
+    {
+      title: 'Census Tract: 429.01',
+      content: ``,
+      bounds: [[10.546875, 48.253941, 36.298828, 57.774518]],
+      features: L.geoJSON(slides, {
+        filter: (feature) => feature.properties.title === 'Census Tract:429.01',
+        onEachFeature(feature, layer) {
+          layer.setStyle({  color: '##fb2828', fillOpacity: 0.4 });
+          layer.bindPopup(`<h4><strong>${feature.properties.NAME}</strong></h4>`);
+        },
+      }),
+    },
+    {
+      title: 'Norwood',
+      content: `Laals 2020 Demographic Survey was born as a result of a lack of data that presently exists about this community, and was conducted alongside Laals efforts to count the Bangladeshi population in the 2020 Census. Prior to this phone banking initiative, hundreds of families in the district remained unrepresented and unaccounted for when it came to federal and state funding. Laal counted 554 individuals and 132 households in Norwood during their Census 2020 outreach. This was the first quantitative research conducted by a CBO about the Bangladeshi community in Norwood.`,
+      bounds: [[10.546875, 48.253941, 36.298828, 57.774518]],
+      features: L.geoJSON(slides, {
+        filter: (feature) => feature.properties.title === 'Norwood',
         onEachFeature(feature, layer) {
           layer.setStyle({  color: '##fb2828', fillOpacity: 0.4 });
           layer.bindPopup(`<h4><strong>${feature.properties.NAME}</strong></h4>`);
