8e6a87a255d9437585f2007fa92cae3cac4ac284
sighuh <ssheth112@gmail.com>
2022-05-11 13:05:30 -0400

    added circle

--- final.js
+++ final.js
@@ -7,6 +7,13 @@ var popup = L.popup()
     .setContent('Here is where the Laal Offices are located. <center><img src=laalBaari.png style="width:90px;height:100px;"></center>')
     .openOn(map);
 
+let laalOffice = L.circle([40.87369498325355, -73.880724989077587], {
+  color: "red",
+  fillColor: "red",
+  fillOpacity: "0.3",
+  radius: 3
+}).addTo(map)
+
 L.tileLayer('https://api.mapbox.com/styles/v1/saisheth/cl1nsj746003g15nz6hd6pqvs/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic2Fpc2hldGgiLCJhIjoiY2wwcjJscXdyMmdsbDNlcWt0eWQ3NHh4bCJ9._YM5R9AGKiS7q_v80O8NVQ', {
 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
